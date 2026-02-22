8e1ee298cbb5a8b6a3ee8c5198dc9cdc138a3598
M1ssshka <anim66056@gmail.com>
2026-02-22 14:56:33 +0400

    feat: добавлены несколько тестовых API и дополнены схемы постов

--- src/api/posts.py
+++ src/api/posts.py
@@ -1,21 +1,74 @@
-from datetime import datetime
-from fastapi import APIRouter
-from pydantic.types import SecretStr
-from src.schemas.posts import Post
-from src.schemas.users import User
+from fastapi import APIRouter, status, HTTPException
+
+from src.schemas.posts import (
+    PostCreateSchema,
+    PostUpdateSchema,
+    PostResponseSchema,
+)
 
 router = APIRouter()
 
+fake_db = []
 
-@router.get('/posts')
-def get_posts() -> Post:
-    return Post(
-        created_at=datetime.now(),
-        is_published=True,
-        title='string',
-        text='string',
-        pub_date=datetime.now(),
-        author=User(username='string', password=SecretStr('string')),
-        location=None,
-        category=None,
-    )
+
+@router.get('/post/{post_id}')
+def get_post(post_id: int):
+    if post_id < len(fake_db):
+        return fake_db[post_id]
+    else:
+        raise HTTPException(
+            detail='Пост не найден',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+
+
+@router.post(
+    '/post/create',
+    status_code=status.HTTP_201_CREATED,
+    response_model=PostResponseSchema,
+)
+def create_post(post: PostCreateSchema) -> dict:
+    response = {
+        'id': len(fake_db),
+        'title': post.title,
+        'text': post.text,
+        'pub_date': post.pub_date,
+        'author': post.author,
+        'location': post.location,
+        'category': post.category,
+        'created_at': post.created_at,
+        'is_published': post.is_published,
+    }
+    fake_db.append(response)
+    return PostResponseSchema.model_validate(obj=response)
+
+
+@router.put(
+    '/posts/{post_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=PostResponseSchema,
+)
+def update_post(post_id: int, post: PostUpdateSchema) -> dict:
+    if post_id >= len(fake_db):
+        raise HTTPException(
+            detail='Пост не найден',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    response = fake_db[post_id]
+    response['title'] = post.title
+    response['text'] = post.text
+    response['location'] = post.location
+    response['category'] = post.category
+    response['is_published'] = post.is_published
+    return response
+
+
+@router.delete('/posts/{post_id}', status_code=status.HTTP_200_OK)
+def delete_post(post_id: int):
+    if post_id >= len(fake_db):
+        raise HTTPException(
+            detail='Пост не найден',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    fake_db.pop(post_id)
+    return {'message': 'Пост успешно удален'}
--- src/schemas/posts.py
+++ src/schemas/posts.py
@@ -1,6 +1,6 @@
 from pydantic import Field
 from datetime import datetime
-from fastapi import UploadFile, File
+# from fastapi import UploadFile, File
 
 from src.schemas.users import User
 from src.schemas.locations import Location
@@ -8,7 +8,7 @@ from src.schemas.categories import Category
 from src.schemas.base import BaseCreatedAt, BasePublished
 
 
-class Post(BasePublished, BaseCreatedAt):
+class PostCreateSchema(BasePublished, BaseCreatedAt):
     title: str = Field(..., max_length=256, title='Заголовок')
     text: str = Field(..., title='Текст')
     pub_date: datetime = Field(
@@ -16,8 +16,36 @@ class Post(BasePublished, BaseCreatedAt):
         title='Дата и время публикации',
         description='Если установить дату и время в будущем — можно делать отложенные публикации.',
     )
-
     author: User = Field(..., title='Автор публикации')
     location: Location | None = Field(None, title='Местоположение')
     category: Category | None = Field(..., title='Категория')
-    image: UploadFile | None = File(None, title='post_images')
+    # image: UploadFile | None = File(None, title='Фото')
+
+
+class PostUpdateSchema(BasePublished, BaseCreatedAt):
+    title: str = Field(..., max_length=256, title='Заголовок')
+    text: str = Field(..., title='Текст')
+    pub_date: datetime = Field(
+        ...,
+        title='Дата и время публикации',
+        description='Если установить дату и время в будущем — можно делать отложенные публикации.',
+    )
+    author: User = Field(..., title='Автор публикации')
+    location: Location | None = Field(None, title='Местоположение')
+    category: Category | None = Field(..., title='Категория')
+    is_published: bool = Field(True, title='Опубликовано')
+
+
+class PostResponseSchema(BasePublished, BaseCreatedAt):
+    id: int = Field(..., title='ID')
+    title: str = Field(..., max_length=256, title='Заголовок')
+    text: str = Field(..., title='Текст')
+    pub_date: datetime = Field(
+        ...,
+        title='Дата и время публикации',
+        description='Если установить дату и время в будущем — можно делать отложенные публикации.',
+    )
+    author: User = Field(..., title='Автор публикации')
+    location: Location | None = Field(None, title='Местоположение')
+    category: Category | None = Field(..., title='Категория')
+    # image: str | None = Field(None, title='Путь к изображению')
