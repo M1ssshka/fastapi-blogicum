ff3725c7b9358eb881c3959f086ff8ab27bed214
M1ssshka <anim66056@gmail.com>
2026-04-02 19:07:35 +0400

    refactor: реорганизация структуры проекта с fastapi_app

--- fastapi_app/src/infrastructure/sqlite/database.py
+++ fastapi_app/src/infrastructure/sqlite/database.py
@@ -9,8 +9,8 @@ from sqlalchemy.ext.declarative import declarative_base
 class Database:
     def __init__(self):
         # Получаем путь к корню проекта (4 уровня вверх от database.py)
-        BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-        db_path = BASE_DIR / "db.sqlite3"
+        BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
+        db_path = BASE_DIR / 'db.sqlite3'
         self._db_url = f'sqlite:///{db_path}'
         self._engine = create_engine(self._db_url)
 
